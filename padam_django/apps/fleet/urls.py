c25cd16bfa700c9dc789ab0db16a26dee6e8e474
abarreaux <alexis.barreaux@gmail.com>
2023-06-20 14:39:45 +0200

    FIrst crude working form

--- padam_django/apps/fleet/urls.py
+++ padam_django/apps/fleet/urls.py
@@ -6,6 +6,7 @@ app_name = "fleet"
 urlpatterns = [
     path("", views.index, name="index"),
     path("place/", views.place_choosing, name="place_choosing"),
+    path("chose_place/", views.place_choice_handling, name="chose_place"),
     path(
         "place/<int:place_id>/", views.place_stops_listing, name="place_stops_listing"
     ),
--- padam_django/apps/fleet/views/place_views.py
+++ padam_django/apps/fleet/views/place_views.py
@@ -1,17 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from ..utils import get_ordered_future_stops_to_place
 from padam_django.apps.geography.models import Place
 
 
-def place_choosing(request) -> HttpResponse:
+def place_choosing(request: HttpRequest) -> HttpResponse:
     places = Place.objects.all()
     print(places)
     return render(request, "fleet/place_choosing.html", {"places": places})
 
 
-def place_stops_listing(request, place_id) -> HttpResponse:
+def place_stops_listing(request: HttpRequest, place_id: int) -> HttpResponse:
     place: Place = get_object_or_404(Place, pk=place_id)
 
     stops_at_place = get_ordered_future_stops_to_place(place=place)
@@ -19,3 +20,9 @@ def place_stops_listing(request, place_id) -> HttpResponse:
     context = {"stops_at_place": stops_at_place, "place": place}
 
     return render(request, "fleet/place_stops_listing.html", context)
+
+
+def place_choice_handling(request: HttpRequest) -> HttpResponseRedirect:
+    return HttpResponseRedirect(
+        reverse("fleet:place_stops_listing", args=(request.POST["place_id"],))
+    )
